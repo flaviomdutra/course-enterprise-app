f68c1b6bbb116f18610919e4cd58bcffe42b7151
Flávio Dutra <flaviomdutra@outlook.com>
2025-03-22 23:01:51 -0300

    implement video upload functionality with Prisma integration and add related tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,17 @@
-import { BadRequestException, Controller, Get, HttpCode, HttpStatus, Post, Req, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { PrismaService } from '@src/prisma.service';
 import { randomUUID } from 'crypto';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
@@ -7,7 +19,10 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -49,10 +64,35 @@ export class AppController {
   )
   async uploadVideo(
     @Req() _req: Request,
+    @Body()
+    contentData: {
+      title: string;
+      description: string;
+    },
     @UploadedFiles()
     files: { video?: Express.Multer.File[]; thumbnail?: Express.Multer.File[] },
-  ): Promise<string> {
-    console.log(files);
-    return 'video uploaded';
+  ): Promise<any> {
+    const videoFile = files.video?.[0];
+    const thumbnailFile = files.thumbnail?.[0];
+
+    if (!videoFile || !thumbnailFile) {
+      throw new BadRequestException(
+        'Both video and thumbnail files are required.',
+      );
+    }
+
+    return await this.prismaService.video.create({
+      data: {
+        id: randomUUID(),
+        title: contentData.title,
+        description: contentData.description,
+        url: videoFile.path,
+        thumbnailUrl: thumbnailFile.path,
+        sizeInKb: videoFile.size,
+        duration: 100,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
   }
 }
