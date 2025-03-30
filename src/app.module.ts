c440e0905b2a33b22cbb9b4e2358f7d0db6ceee8
Flávio Dutra <flaviomdutra@outlook.com>
2025-03-30 09:36:34 -0300

    refactor project structure and implement video content management with streaming support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ContentManagementService } from './core/content-management.service';
+import { MediaPlayerService } from './core/media-player.service';
+import { ContentController } from './http/controller/content.controller';
 import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [ContentController],
+  providers: [PrismaService, ContentManagementService, MediaPlayerService],
 })
 export class AppModule {}
--- src/http/controller/content.controller.ts
+++ src/http/controller/content.controller.ts
@@ -6,7 +6,6 @@ import {
   Header,
   HttpCode,
   HttpStatus,
-  NotFoundException,
   Param,
   Post,
   Req,
@@ -15,25 +14,20 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
-import { PrismaService } from '@src/prisma.service';
+import { ContentManagementService } from '@src/core/content-management.service';
+import { MediaPlayerService } from '@src/core/media-player.service';
 import { randomUUID } from 'crypto';
-import type { Request, Response } from 'express';
+import { Request, Response } from 'express';
 import fs from 'fs';
 import { diskStorage } from 'multer';
 import path, { extname } from 'path';
-import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
+@Controller('content')
+export class ContentController {
   constructor(
-    private readonly appService: AppService,
-    private readonly prismaService: PrismaService,
+    private readonly contentManagementService: ContentManagementService,
+    private readonly mediaPlayerService: MediaPlayerService,
   ) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 
   @Post('video')
   @HttpCode(HttpStatus.CREATED)
@@ -54,10 +48,14 @@ export class AppController {
             );
           },
         }),
-        fileFilter: (req, file, cb) => {
+        fileFilter: (_req, file, cb) => {
           if (file.mimetype !== 'video/mp4' && file.mimetype !== 'image/jpeg') {
-            req.fileValidationError = 'UNSUPPORTED_FILE_TYPE';
-            return cb(null, false);
+            return cb(
+              new BadRequestException(
+                'Invalid file type. Only video/mp4 and image/jpeg are supported.',
+              ),
+              false,
+            );
           }
           return cb(null, true);
         },
@@ -65,7 +63,7 @@ export class AppController {
     ),
   )
   async uploadVideo(
-    @Req() req: Request & { fileValidationError?: string },
+    @Req() _req: Request,
     @Body()
     contentData: {
       title: string;
@@ -77,33 +75,18 @@ export class AppController {
     const videoFile = files.video?.[0];
     const thumbnailFile = files.thumbnail?.[0];
 
-    if (
-      req.fileValidationError &&
-      req.fileValidationError === 'UNSUPPORTED_FILE_TYPE'
-    ) {
-      throw new BadRequestException(
-        'Invalid file type. Only video/mp4 and image/jpeg are supported.',
-      );
-    }
-
     if (!videoFile || !thumbnailFile) {
       throw new BadRequestException(
         'Both video and thumbnail files are required.',
       );
     }
 
-    return await this.prismaService.video.create({
-      data: {
-        id: randomUUID(),
-        title: contentData.title,
-        description: contentData.description,
-        url: videoFile.path,
-        thumbnailUrl: thumbnailFile.path,
-        sizeInKb: videoFile.size,
-        duration: 100,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
+    return await this.contentManagementService.createContent({
+      title: contentData.title,
+      description: contentData.description,
+      url: videoFile.path,
+      thumbnailUrl: thumbnailFile.path,
+      sizeInKb: videoFile.size,
     });
   }
 
@@ -113,21 +96,17 @@ export class AppController {
     @Param('videoId') videoId: string,
     @Req() req: Request,
     @Res() res: Response,
-  ): Promise<any> {
-    const video = await this.prismaService.video.findUnique({
-      where: {
-        id: videoId,
-      },
-    });
+  ) {
+    const url = await this.mediaPlayerService.prepareStreaming(videoId);
 
-    if (!video) {
-      throw new NotFoundException('Video not found');
+    if (!url) {
+      return res.sendStatus(HttpStatus.NOT_FOUND);
     }
-
-    const videoPath = path.join('.', video.url);
+    const videoPath = path.join('.', url);
     const fileSize = fs.statSync(videoPath).size;
 
     const range = req.headers.range;
+
     if (range) {
       const parts = range.replace(/bytes=/, '').split('-');
       const start = parseInt(parts[0], 10);
@@ -143,11 +122,13 @@ export class AppController {
         'Content-Type': 'video/mp4',
       });
 
-      return file.pipe(res);
+      file.pipe(res);
+    } else {
+      res.writeHead(HttpStatus.OK, {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      });
+      fs.createReadStream(videoPath).pipe(res);
     }
-    res.writeHead(HttpStatus.OK, {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    });
   }
 }
