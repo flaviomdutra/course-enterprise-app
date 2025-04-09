772d80f329733779beb4bce0d883c013ced52942
Flávio Dutra <flaviomdutra@outlook.com>
2025-04-09 17:07:05 -0300

    chore: remove Prisma integration and add TypeORM support
    - Removed PrismaService and its references from app.module.ts.
    - Added TypeORM and typeorm-extension dependencies in package.json.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ContentManagementService } from '@src/core/service/content-management.s
 import { MediaPlayerService } from '@src/core/service/media-player.service';
 import { ContentController } from '@src/http/rest/controller/content.controller';
 import { MediaPlayerController } from '@src/http/rest/controller/media-player.controller';
-import { PrismaService } from '@src/persistence/prisma/prisma.service';
 import { ContentRepository } from '@src/persistence/repository/content.repository';
 import { VideoRepository } from '@src/persistence/repository/video.repository';
 import { ConfigModule } from './infra/module/config/config.module';
@@ -12,7 +11,6 @@ import { ConfigModule } from './infra/module/config/config.module';
   imports: [ConfigModule.forRoot()],
   controllers: [ContentController, MediaPlayerController],
   providers: [
-    PrismaService,
     ContentManagementService,
     MediaPlayerService,
     ContentRepository,
