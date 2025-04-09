2dc5c90e2ba63669268a7f9332188ed574cf2c7c
Flávio Dutra <flaviomdutra@outlook.com>
2025-04-08 22:03:57 -0300

    feat: refactor video handling by adding MediaPlayerController and VideoRepository, and implement end-to-end tests for video upload and streaming

--- src/__test__/content.e2e-spec.ts
+++ src/__test__/content.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '@src/app.module';
-import { PrismaService } from '@src/persistence/prisma/prisma.service';
+import { VideoRepository } from '@src/persistence/repository/video.repository';
 import fs from 'fs';
 import request from 'supertest';
 
 describe('ContentController (e2e)', () => {
   let module: TestingModule;
   let app: INestApplication;
-  let prismaService: PrismaService;
+  let videoRepository: VideoRepository;
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
@@ -18,7 +18,7 @@ describe('ContentController (e2e)', () => {
     app = module.createNestApplication();
     await app.init();
 
-    prismaService = module.get<PrismaService>(PrismaService);
+    videoRepository = module.get<VideoRepository>(VideoRepository);
   });
 
   beforeEach(async () => {
@@ -28,7 +28,7 @@ describe('ContentController (e2e)', () => {
   });
 
   afterEach(async () => {
-    await prismaService.video.deleteMany();
+    await videoRepository.clear();
   });
 
   afterAll(async () => {
@@ -59,9 +59,6 @@ describe('ContentController (e2e)', () => {
             title: video.title,
             description: video.description,
             url: expect.stringContaining('mp4'),
-            thumbnailUrl: expect.stringContaining('jpg'),
-            sizeInKb: video.sizeInKb,
-            duration: video.duration,
           });
         });
     });
@@ -114,38 +111,6 @@ describe('ContentController (e2e)', () => {
           error: 'Bad Request',
           statusCode: 400,
         });
-    });
-  });
-
-  describe('GET /stream/:videoId', () => {
-    it('streams a video', async () => {
-      const { body: sampleVideo } = await request(app.getHttpServer())
-        .post('/content/video')
-        .attach('video', './test/fixtures/sample.mp4')
-        .attach('thumbnail', './test/fixtures/sample.jpg')
-        .field('title', 'Test Video')
-        .field('description', 'This is a test video')
-        .expect(HttpStatus.CREATED);
-
-      const fileSize = 1430145;
-      const range = `bytes=0-${fileSize - 1}`;
-
-      const response = await request(app.getHttpServer())
-        .get(`/content/stream/${sampleVideo.id}`)
-        .set('Range', range)
-        .expect(HttpStatus.PARTIAL_CONTENT);
-
-      expect(response.headers['content-range']).toBe(
-        `bytes 0-${fileSize - 1}/${fileSize}`,
-      );
-      expect(response.headers['accept-ranges']).toBe('bytes');
-      expect(response.headers['content-length']).toBe(String(fileSize));
-      expect(response.headers['content-type']).toBe('video/mp4');
-    });
-    it('returns 404 if the video is not found', async () => {
-      await request(app.getHttpServer())
-        .get('/content/stream/45705b56-a47f-4869-b736-8f6626c940f8')
-        .expect(HttpStatus.NOT_FOUND);
     });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ContentManagementService } from './core/service/content-management.service';
-import { MediaPlayerService } from './core/service/media-player.service';
-import { ContentController } from './http/rest/controller/content.controller';
-import { PrismaService } from './persistence/prisma/prisma.service';
+import { ContentManagementService } from '@src/core/service/content-management.service';
+import { MediaPlayerService } from '@src/core/service/media-player.service';
+import { ContentController } from '@src/http/rest/controller/content.controller';
+import { MediaPlayerController } from '@src/http/rest/controller/media-player.controller';
+import { PrismaService } from '@src/persistence/prisma/prisma.service';
+import { ContentRepository } from '@src/persistence/repository/content.repository';
+import { VideoRepository } from '@src/persistence/repository/video.repository';
 
 @Module({
   imports: [],
-  controllers: [ContentController],
-  providers: [PrismaService, ContentManagementService, MediaPlayerService],
+  controllers: [ContentController, MediaPlayerController],
+  providers: [
+    PrismaService,
+    ContentManagementService,
+    MediaPlayerService,
+    ContentRepository,
+    VideoRepository,
+  ],
 })
 export class AppModule {}
