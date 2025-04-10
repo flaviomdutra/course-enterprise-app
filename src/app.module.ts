fdba9b2b60a4a789b785ffb19bf2811d0ef31c5a
Flávio Dutra <flaviomdutra@outlook.com>
2025-04-10 18:33:32 -0300

    feat: integrate GraphQL module and identity module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { ContentModule } from '@contentModule/content.module';
+import { IdentityModule } from '@identityModule/identity.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [ContentModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ContentModule,
+    IdentityModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+  ],
 })
 export class AppModule {}
