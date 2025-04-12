9916c6747bd6c38cb392905ccf57b101007c6a85
Flávio Dutra <flaviomdutra@outlook.com>
2025-04-11 23:22:33 -0300

    fix(client): update HttpClient import path to use shared module

--- src/module/content/http/rest/client/external-movie-rating/external-movie-rating.client.ts
+++ src/module/content/http/rest/client/external-movie-rating/external-movie-rating.client.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@contentModule/infra/http/client/http.client';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@sharedModules/config/service/config.service';
+import { HttpClient } from '@sharedModules/http-client/client/http.client';
 
 interface ApiResponse<T extends Record<string, any>> {
   results: Array<T>;
