3c76c49e3d3f6c527d777613629e2e51fb4292b8
Flávio Dutra <flaviomdutra@outlook.com>
2025-04-11 21:46:55 -0300

    feat(billing): add BillingPublicApiProvider for subscription status validation

--- src/module/billing/billing.module.ts
+++ src/module/billing/billing.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
 import { SubscriptionService } from '@billingModule/core/service/subscription.service';
 import { SubscriptionController } from '@billingModule/http/rest/controller/subscription.controller';
 import { BillingPersistenceModule } from '@billingModule/persistence/billing-persistence.module';
+import { Module } from '@nestjs/common';
+import { BillingPublicApiProvider } from './integration/provider/public-api.provider';
 
 @Module({
   imports: [BillingPersistenceModule],
-  providers: [SubscriptionService],
+  providers: [SubscriptionService, BillingPublicApiProvider],
   controllers: [SubscriptionController],
-  exports: [],
+  exports: [BillingPublicApiProvider],
 })
 export class BillingModule {}
