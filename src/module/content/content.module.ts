ec12564590ddb0be0fcf5e819008b59fd3c1ad26
Flávio Dutra <flaviomdutra@outlook.com>
2025-05-25 12:53:51 -0300

    feat: update Docker configuration and add video processor worker module

--- src/module/content/content.module.ts
+++ src/module/content/content.module.ts
@@ -1,13 +1,8 @@
 import { ContentAdminModule } from '@contentModule/admin/content-admin.module';
 import { ContentCatalogModule } from '@contentModule/catalog/content-catalog.module';
-import { ContentVideoProcessorModule } from '@contentModule/video-processor/content-video-processor.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [
-    ContentAdminModule,
-    ContentVideoProcessorModule,
-    ContentCatalogModule,
-  ],
+  imports: [ContentAdminModule, ContentCatalogModule],
 })
 export class ContentModule {}
